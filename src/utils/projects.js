58f735d6458e7f4c2189426b69f75b4ee298f308
Wesley Costa <wesleyeugenio3@gmail.com>
2025-01-03 17:27:50 -0300

    fix: add and change projects infos

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,4 +1,4 @@
-import EncurtadorLinks from "../assets/encurtador-links.png";
+import ShortenerLinks from "../assets/shortener-links.png";
 import DinDin from "../assets/din-din.png";
 import Pagamentos from "../assets/pagamentos.png";
 import VirtualRoom from "../assets/virtual-rooms.png";
@@ -7,23 +7,23 @@ export const projects = [
   {
     title: "Meu Link",
     text: "Se trata de uma aplicação que faz o encurtamento dos seus links e com o auxílio da API do bit.ly.",
-    link: "https://eugenio-cyber.github.io/reactjs-encurtador-links/",
-    img: EncurtadorLinks,
-    technologies: ["ReactJS", "React Router", "React Icons"],
+    link: "https://eugenio-cyber.github.io/reactjs-link-shortener",
+    img: ShortenerLinks,
+    technologies: ["ReactJS", "React Router", "React Icons", "GitHub Pages"],
   },
   {
     title: "DinDin",
     text: "O objetivo desse projeto foi o desenvolvimento duma aplicação para controles de finanças pessoais.",
     link: "https://dindin-app-three.vercel.app/",
     img: DinDin,
-    technologies: ["ReactJS", "NodeJS", "PostgreSQL"],
+    technologies: ["ReactJS", "NodeJS", "PostgreSQL", "Vercel"],
   },
   {
     title: "PayPay",
     text: "Esse projeto se trata de uma aplicação capaz de gerenciar devedores, com várias funcionalidade implementadas.",
     link: "https://paypay-app.vercel.app/",
     img: Pagamentos,
-    technologies: ["ReactJS", "Material UI", "NodeJS", "Vercel", "PostgreSQL"],
+    technologies: ["ReactJS", "Material UI", "NodeJS", "PostgreSQL", "Vercel"],
   },
   {
     title: "Virtual Rooms",
