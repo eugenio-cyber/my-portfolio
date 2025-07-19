f965cfb7575b4b82f5dfbaf0ea5bf0ec2f97fff8
Wesley Costa <wesleyeugenio3@gmail.com>
2025-07-19 14:07:03 -0300

    feat: add new company experience

--- src/utils/companies.js
+++ src/utils/companies.js
@@ -1,12 +1,18 @@
 const companies = [
   {
+    occupation: "Desenvolvedor Full-Stack",
+    name: "Strelo",
+    period: "Jan 2025 - o momento",
+    description:
+      "Atuo adicionando funcionalidades e dando manutenção ao projeto da empresa de maneira full-stack, utilizando meus conhecimentos em: NextJS, Material UI, TypeScript, GraphQL, MongoDB, Vercel, versionamento com Git e GitHub.",
+  },
+  {
     occupation: "Desenvolvedor Full-Stack Jr",
     name: "erural",
     period: "Dez 2022 - Set 2024",
     description:
       "Atuava desenvolvendo novas funcionalidades e dando manutenção ao projeto da empresa tanto no frontend como também no backend, e para isso utilizo meus conhecimentos em: HTML, CSS, JavaScript, Ruby, VueJS, Ruby on Rails, Docker, versionamento com Git e GitHub.",
   },
-
   {
     occupation: "Desenvolvedor Full-Stack",
     name: "Cubos Academy",
@@ -14,7 +20,6 @@ const companies = [
     description:
       "Atuava no desenvolvimento de novas funcionalidades com: ReactJS, Typescript e SASS para o frontend. Mas também atuei no backend com NodeJS e Typescript. Por meio dessa experiencia aprimorei meu trabalho em equipe e minha gestão de entregas.",
   },
-
   {
     occupation: "Estagiário Front-End",
     period: "Nov 2021 - Dez 2021",
